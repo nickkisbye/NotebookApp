eef8fcdeae4741c3c78961200095056f5be248a2
Nick Hansen <44166062+nickkisbye@users.noreply.github.com>
2020-02-17 11:18:07 +0100

    Notebook Week 8 assignment
    + button to add a headline to a listview
    The Listview shows the headlines
    when you press a headline, you get to a detail page where you can edit and save it.

---
+++ Week 8/src/main/java/com/example/mynotebook/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.mynotebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.mynotebook.Models.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    public static List<String> headlines = new ArrayList<>();
+    public static List<Note> noteList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, headlines);
+        listView.setAdapter(adapter);
+
+        /**
+         * Activates when an item in the ListView is clicked. the onItemClick will provide us with the index of the item (int i),
+         * so I parse the integer to the getNoteDetails method.
+         */
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                getNoteDetails(i);
+            }
+        });
+    }
+
+    /**
+     * This method runs a new intend and switches the screen to the NoteDetails Activity.
+     * intent.putExtra provides the intend object with the id of the item, which is used to reference the Note object
+     * in the noteList.
+     * @param i
+     */
+    public void getNoteDetails(int i) {
+        Intent intent = new Intent(this, NoteDetails.class);
+        intent.putExtra("noteId", i);
+        startActivity(intent);
+    }
+
+    /**
+     * Creates a new Note object.
+     * @param view
+     */
+    public void addNote(View view) {
+        Note note = new Note();
+
+        note.setId(headlines.size() + 1);
+        note.setTitle("Headline " + (headlines.size() + 1));
+        note.setBody("");
+
+        noteList.add(note);
+        headlines.add(note.getTitle());
+        adapter.notifyDataSetChanged();
+    }
+
+}
---
+++ Week 8/src/main/java/com/example/mynotebook/NoteDetails.java
@@ -0,0 +1,46 @@
+package com.example.mynotebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.mynotebook.Models.Note;
+
+public class NoteDetails extends AppCompatActivity {
+
+    private Note note;
+    private EditText headline;
+    private EditText body;
+    private int noteId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId", 0);
+        note = MainActivity.noteList.get(noteId);
+
+        headline = findViewById(R.id.editText);
+        body = findViewById(R.id.editText2);
+
+        headline.setText(note.getTitle());
+        body.setText(note.getBody());
+    }
+
+    /**
+     * Edit the note object and headline and switches back to the MainActivity.
+     * @param view
+     */
+    public void saveNote(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        note.setTitle(headline.getText().toString());
+        note.setBody(body.getText().toString());
+        MainActivity.headlines.set(noteId, headline.getText().toString());
+        startActivity(intent);
+    }
+}
